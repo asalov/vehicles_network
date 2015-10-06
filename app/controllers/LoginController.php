9c722248d7812aa5468766bdab1f7e4c73244483
Aleksandar Salov <aleksandarsalov@yahoo.com>
2015-10-06 15:11:12 +0200

    New change

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller{
 			$this->view->render('login/index');
 		}
 
-		// Redirect depending on role
+		// Redirect depending on the role of the user
 		redirect(PATH);
 	}
 
